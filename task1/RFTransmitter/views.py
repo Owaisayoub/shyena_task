35fc958529b796170eb2c9bcf2da10db85921e2d
owais_11 <oemollic@gmail.com>
2024-03-15 12:40:04 +0530

    refactored the code

--- task1/RFTransmitter/views.py
+++ task1/RFTransmitter/views.py
@@ -8,9 +8,34 @@ from .serializer import *
 from django.views.decorators.csrf import csrf_exempt
 
 
+class Generic:
+    def update_config_table(self, current_status, process_id):
+            last_user = ConfigTable.objects.last()
+            last_user.pid = process_id
+            last_user.status = current_status
+            last_user.save()
+            return last_user
+    def store_log(self, action, log_data):
+        log_data["action"] = action
+        log_data = LogTableSeralizer(data = log_data)
+        if log_data.is_valid():
+            log_data.save()
+            return True,log_data.data
+        return False,log_data.errors 
+    def store_new_configs(self, status, process_id, data):
+        config_data = data
+        config_data["status"] = status
+        config_data["pid"] = process_id
+        config_data = ConfigTableSeralizer(data=config_data)
+        if config_data.is_valid():
+            config_data.save()
+            return True, config_data.data
+        
+        return False, config_data.errors
+        
 
-@method_decorator(csrf_exempt, name='dispatch')
-class SaveView(APIView):
+    
+class SaveView(APIView,Generic):
     
     def get(self, request):
         obj = ConfigTable.objects.last()
@@ -26,23 +51,18 @@ class SaveView(APIView):
                 'message': 'data Already present'
             })
         if last_row:
+
             process_id = last_row.pid
-            last_row.pid = None
-            last_row.status = "idle"
-            last_row.save()
-        data_for_log_table = request.data
-        data_for_log_table["action"] = 'save'
-        config_data_serilized = ConfigTableSeralizer(data  = config_data)
-        if config_data_serilized.is_valid():
-            config_data_serilized.save()
-            log_table_serializer = LogTableSeralizer(data = data_for_log_table)
-            if log_table_serializer.is_valid():
-                log_table_serializer.save()
-                response_obj = config_data_serilized.data
-                del response_obj["pid"]
-                return Response(response_obj)
-            return Response(log_table_serializer.errors)
-        return Response(config_data_serilized.errors)
+            #kill the process id
+            updated_last_row = self.update_config_table('idle', None)
+
+        log_valid, log_res = self.store_log('save',request.data)
+        config_valid, config_res = self.store_new_configs('idle', None, request.data)
+        if not config_valid:
+            return Response(config_res)
+        if not log_valid:
+            return Response(log_res)
+        return Response(config_res)
             
     
     def compare(self, dict1, dict2):
@@ -54,12 +74,12 @@ class SaveView(APIView):
                 return False
         return True
     
-class StartView(APIView):
+class StartView(APIView, Generic):
 
     def get(self, request):
         data = ConfigTable.objects.last()
-        data_serialized = ConfigTableSeralizer(data_serialized)
-        return Response(data_serialized.data)
+        data = ConfigTableSeralizer(data)
+        return Response(data.data)
 
     def post(self, request):
         data_in_db = ConfigTable.objects.all()
@@ -69,32 +89,19 @@ class StartView(APIView):
         user_table_last_row =ConfigTableSeralizer(ConfigTable.objects.last()).data
         log_table_last_row  = LogTableSeralizer(LogTable.objects.last()).data
 
-
-        if not user_table_last_row:
-            return 
         
-        if log_table_last_row["action"] == "save" and self.compare(user_data, user_table_last_row):
-            
+        if log_table_last_row["action"] == "save" and self.compare(user_data, user_table_last_row): 
             #Run the subprocess
             process_id = self.run_sub_process ()     
-
             #update the last row of the user table
-            last_user = ConfigTable.objects.last()
-            last_user.pid = process_id
-            last_user.status = 'active'
-            last_user.save()
-            last_user_serialized = ConfigTableSeralizer(last_user)
-
+            last_user = ConfigTableSeralizer(self.update_config_table('start',process_id))
             #store the log
-
-            log_data = request.data
-            log_data["action"] = 'start'
-            log_data_serialized = LogTableSeralizer(data = log_data)
-            if log_data_serialized.is_valid():
-                log_data_serialized.save()
-                return Response(last_user_serialized.data)
-            return Response(log_data_serialized.errors)
-
+            save_success, res = self.store_log('start', request.data)
+            if save_success:
+                return Response(last_user.data)
+            else:
+                return Response(res)
+            
         if log_table_last_row["action"] == "save" and not self.compare(user_data, user_table_last_row):
             res = self.save_start_different_data(request)
             return Response(res)
@@ -104,7 +111,6 @@ class StartView(APIView):
             return Response({
                 "message": "system already running..."
             })
-        # print("passed the same data with action start")
 
         if log_table_last_row["action"] == "start" and not self.compare(user_data, user_table_last_row):
             res = self.save_start_different_data(request)
@@ -119,84 +125,55 @@ class StartView(APIView):
             process_id = self.run_sub_process()
 
             #Change the status and store pid
-            config_last_row.pid = process_id
-            config_last_row.status = "start"
-            config_last_row.save()
-            config_last_row_serialized =ConfigTableSeralizer(config_last_row)
+            config_table_updated_last = self.update_config_table('active', process_id)
+            config_last_row =ConfigTableSeralizer(config_table_updated_last)
 
             #save the logs
-            log_data = request.data 
-            log_data["action"] = 'start'
-            log_data_serialized = LogTableSeralizer(data= log_data)
-            if log_data_serialized.is_valid():
-                log_data_serialized.save()
-                return Response(config_last_row_serialized.data)
-            return Response({"message": "something went wrong"})
+            valid, res = self.store_log('start', request.data)
+            if not valid:
+                return Response({"message": "something went wrong"})
+            
+            return Response(config_last_row.data)
 
         if log_table_last_row["action"] == 'stop' and not self.compare(user_data,user_table_last_row):
-            print('###############')
             #run new subprocess
             process_id = self.run_sub_process()
             #Store new configs
-            config_data = request.data
-            config_data["status"] = 'active'
-            config_data["pid"] = process_id
-            config_data_serialized = ConfigTableSeralizer(data=config_data)
-            if config_data_serialized.is_valid():
-                config_data_serialized.save()
-            else:
-                return Response(config_data_serialized.errors)
+            valid ,config_res = self.store_new_configs('active',process_id,request.data)
+            if not valid:
+                print('not valid')
+                return Response(res)
         
             #Take new Logs
-            log_data = request.data
-            log_data["action"] = 'start'
-            log_data_serialized = LogTableSeralizer(data = log_data)
-            if log_data_serialized.is_valid():
-                log_data_serialized.save()
-                return Response(config_data_serialized.data)
-            return Response({"message": "something went wrong..."})
+            save_success, log_res = self.store_log('start', request.data)
+            if save_success:
+                return Response(config_res)
+            return Response({"message": "something went wrong"})
+
     
     
     def save_start_different_data(self,request):
-
         print('inside the save_start func')
         #Get the last row of config table
         config_table_last_row = ConfigTable.objects.last()
-        if config_table_last_row.status == 'active': 
-        
+        if config_table_last_row.status == 'active':  
             #Get the process Id kill it  make it Null in DB and change  status to idle
             process_id = config_table_last_row.pid
             #kill here
-            config_table_last_row.pid = None
-            config_table_last_row.status = 'idle'
-            config_table_last_row.save()
-
-        #Run new subProcess
-                
+            last_user = self.update_config_table('idle', None)
+        #Run new subProcess            
         process_id = self.run_sub_process()
-
-
         #save new configs in config table along with pid
-        config_data = request.data
-        config_data["pid"]= process_id
-        config_data["status"]= "active"
-
-        config_data_serialized = ConfigTableSeralizer(data =config_data)
-        if config_data_serialized.is_valid():
-            config_data_serialized.save()
-        else:
-            return config_data_serialized.errors
-
+        print('#############')
+        valid, config_res = self.store_new_configs('active', process_id, request.data)
+        print('@@@@@@@@', config_res)
+        if not valid:
+            
+            return res
         #save new log in log table
-        log_data = request.data
-        log_data["action"] = 'start'
-
-        log_data_serialized = LogTableSeralizer(data = log_data)
-        if log_data_serialized.is_valid():
-            log_data_serialized.save()
-            print('save the log data')
-            return config_data_serialized.data
-        return log_data_serialized.errors     
+        valid, res = self.store_log('start', request.data)
+        return config_res
+  
         
     def run_sub_process(self):
         cmd = ["python3", "script.py"]  
@@ -212,7 +189,7 @@ class StartView(APIView):
         return True
     
         
-class StopView(APIView):
+class StopView(APIView, Generic):
     def get():
         pass
     def post(self,request):
@@ -226,19 +203,21 @@ class StopView(APIView):
             return Response({
                 "message": "System is already idle"
             })
-        
-        user_data = request.data
-        user_data["action"]='stop'
         process_id=user_table_last_row.pid
-        user_table_last_row.pid=None
-        user_table_last_row.status='idle'
-        user_table_last_row.save()
+        updated_last_row = ConfigTableSeralizer(self.update_config_table('idle', None)).data
+        
         #KILL KARNA HEIN
-        log_table_serializer=LogTableSeralizer(data=user_data)
-        if log_table_serializer.is_valid():
-            log_table_serializer.save()
-            res = log_table_serializer.data
-            res["status"]=user_table_last_row.status
-            return Response(res)
-        return Response(log_table_serializer.errors)
+        data = ConfigTable.objects.last()
+        # data = ConfigTableSeralizer(data).data
+        data_for_log = {
+            "type": data.type,
+            "frequency": data.frequency,
+            "prf": data.prf,
+            "pw":data.pw
+        }
+        valid, log_res = self.store_log('stop',data_for_log)
+        if not valid:
+            return Response(log_res)
+        return Response(updated_last_row)
+
         
